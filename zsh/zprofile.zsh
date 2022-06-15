0368620118e714fc633e4f75e03b9b3c1b20b740
Pierce Darragh <pierce.darragh@gmail.com>
2022-06-15 12:15:06 -0600

    zsh/*: commit old changes

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -1,6 +1,70 @@
-# Path for Pyenv.
-export PYENV_ROOT="$HOME/.pyenv"
-[[ ! -d ${PYENV_ROOT} ]] || export PATH="$PYENV_ROOT/bin:$PATH"
+export HOMEBREW_BASE="/opt/homebrew"
+export HOMEBREW_OPT="$HOMEBREW_BASE/opt"
+export HOMEBREW_LIB="$HOMEBREW_BASE/lib"
+export RUBY_BASE="$HOMEBREW_BASE/opt/ruby"
+export RUBY_EXEC="$RUBY_BASE/bin/ruby"
+export GEM_EXEC="$RUBY_BASE/bin/gem"
+export GEM_DIR="$($GEM_EXEC env gemdir)/bin"
+export SMLNJ_BASE="/usr/local/smlnj"
 
-# Pyenv initalization.
-eval "$(pyenv init --path)"
+sources=(
+    $HOME/.opam/opam-init/init.zsh
+)
+
+for src in $sources; do source $src; done
+
+path_extensions=(
+    $(xcrun --show-sdk-path)/usr/bin
+    $HOME/.cargo/bin
+    $HOME/.ghcup/bin
+    $HOMEBREW_BASE/bin
+    $HOMEBREW_BASE/sbin
+    $HOMEBREW_OPT/llvm/bin
+    $HOMEBREW_OPT/openssl@3/bin
+    $HOMEBREW_OPT/sqlite/bin
+    $RUBY_BASE/bin
+    $GEM_DIR
+    $SMLNJ_BASE/bin
+)
+
+for extension in $path_extensions; do { [[ ! -d $extension ]] || export PATH="$extension:$PATH" }; done
+
+linker_paths=(
+    $(xcrun --show-sdk-path)/usr/lib
+    $HOMEBREW_OPT/apr/lib
+    $HOMEBREW_OPT/apr-util/lib
+    $HOMEBREW_OPT/bzip2/lib
+    $HOMEBREW_OPT/icu4c/lib
+    $HOMEBREW_OPT/libffi/lib
+    $HOMEBREW_OPT/llvm/lib
+    $HOMEBREW_OPT/ncurses/lib
+    $HOMEBREW_OPT/openssl@1.1/lib
+    $HOMEBREW_OPT/openssl@3/lib
+    $HOMEBREW_OPT/readline/lib
+    $HOMEBREW_OPT/zlib/lib
+    $RUBY_BASE/lib
+    $SMLNJ_BASE/bin
+)
+
+for linker_path in $linker_paths; do { [[ ! -d $linker_path ]] || export LDFLAGS="-L$linker_path $LDFLAGS" }; done
+
+libs=(
+    $(xcrun --show-sdk-path)/usr/include
+    $HOMEBREW_OPT/apr/include
+    $HOMEBREW_OPT/apr-util/include
+    $HOMEBREW_OPT/bzip2/include
+    $HOMEBREW_OPT/icu4c/include
+    $HOMEBREW_OPT/libffi/include
+    $HOMEBREW_OPT/llvm/include
+    $HOMEBREW_OPT/ncurses/include
+    $HOMEBREW_OPT/openssl@1.1/include
+    $HOMEBREW_OPT/openssl@3/include
+    $HOMEBREW_OPT/readline/include
+    $HOMEBREW_OPT/zlib/include
+    $RUBY_BASE/include
+)
+
+for lib in $libs; do { [[ ! -d $lib ]] || { export CPPFLAGS="-I$lib $CPPFLAGS"; export CFLAGS="-I$lib $CFLAGS" }}; done
+
+export PYENV_ROOT=$HOME/.pyenv
+eval $(pyenv init --path)
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -103,11 +110,19 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
+# Add support for auto-completions.
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+    autoload -Uz compinit
+    compinit
+fi
+
 # Add autosuggestions.
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Add support for syntax highlighting on the command line.
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
