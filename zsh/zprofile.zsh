bd388277c1a6a9468a0448657cd0cb09f63bb30e
Pierce Darragh <pierce.darragh@gmail.com>
2022-09-07 10:04:17 -0400

    zsh/zprofile: only add items conditionally

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -7,14 +7,17 @@ export GEM_EXEC="$RUBY_BASE/bin/gem"
 export GEM_DIR="$($GEM_EXEC env gemdir)/bin"
 export SMLNJ_BASE="/usr/local/smlnj"
 
+if command -v xcrun &> /dev/null
+then export SDK_PATH="$(xcrun --show-sdk-path)"
+fi
+
 sources=(
     $HOME/.opam/opam-init/init.zsh
 )
 
-for src in $sources; do source $src; done
+for src in $sources; do [[ ! -r $src ]] || source $src; done
 
 path_extensions=(
-    $(xcrun --show-sdk-path)/usr/bin
     $HOME/.cargo/bin
     $HOME/.ghcup/bin
     $HOMEBREW_BASE/bin
@@ -25,12 +28,12 @@ path_extensions=(
     $RUBY_BASE/bin
     $GEM_DIR
     $SMLNJ_BASE/bin
+    $SDK_PATH/usr/bin
 )
 
 for extension in $path_extensions; do { [[ ! -d $extension ]] || export PATH="$extension:$PATH" }; done
 
 linker_paths=(
-    $(xcrun --show-sdk-path)/usr/lib
     $HOMEBREW_OPT/apr/lib
     $HOMEBREW_OPT/apr-util/lib
     $HOMEBREW_OPT/bzip2/lib
@@ -44,12 +47,12 @@ linker_paths=(
     $HOMEBREW_OPT/zlib/lib
     $RUBY_BASE/lib
     $SMLNJ_BASE/bin
+    $SDK_PATH/usr/lib
 )
 
 for linker_path in $linker_paths; do { [[ ! -d $linker_path ]] || export LDFLAGS="-L$linker_path $LDFLAGS" }; done
 
 libs=(
-    $(xcrun --show-sdk-path)/usr/include
     $HOMEBREW_OPT/apr/include
     $HOMEBREW_OPT/apr-util/include
     $HOMEBREW_OPT/bzip2/include
@@ -62,9 +65,13 @@ libs=(
     $HOMEBREW_OPT/readline/include
     $HOMEBREW_OPT/zlib/include
     $RUBY_BASE/include
+    $SDK_PATH/usr/include
 )
 
 for lib in $libs; do { [[ ! -d $lib ]] || { export CPPFLAGS="-I$lib $CPPFLAGS"; export CFLAGS="-I$lib $CFLAGS" }}; done
 
-export PYENV_ROOT=$HOME/.pyenv
-eval $(pyenv init --path)
+if command -v pyenv &> /dev/null
+then
+    export PYENV_ROOT=$HOME/.pyenv
+    eval $(pyenv init --path)
+fi
